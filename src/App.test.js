f7c1f6f5f43022319bb5aa65e6ccfa0c3f1204d5
cyeluri <chaitanya.yeluri@vertiv.com>
2020-06-29 08:05:10 -0400

    updated test.

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 
 test('renders I was changed link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/I was changed/i);
+  const linkElement = getByText('I was changed');
   expect(linkElement).toBeInTheDocument();
 });
 
