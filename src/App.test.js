c8df7ebe9daf5a0fe71e32dcb6b460bdf33fa822
cyeluri <chaitanya.yeluri@vertiv.com>
2020-06-29 07:59:30 -0400

    updated test.

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders I was changed link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/I was changed/i);
   expect(linkElement).toBeInTheDocument();
 });
 
